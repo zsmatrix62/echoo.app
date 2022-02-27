fb065ff12ca6dd7981a0ff21d75e5bb132236673
zs.matrix62 <zs.matrix62@gmail.com>
2022-02-27 21:39:37 +0800

    .

--- web-src/src/app/consts/literals.ts
+++ web-src/src/app/consts/literals.ts
@@ -1,2 +1,2 @@
-export const GITHUB_HOMEPAGE = "https://github.com/echoo-app/echoo-app"
-export const RELEASE_DOWNLOAD_PAGE = "https://github.com/echoo-app/echoo-app/releases"
+export const GITHUB_HOMEPAGE = "https://github.com/zsmatrix62/echoo-app"
+export const RELEASE_DOWNLOAD_PAGE = "https://github.com/zsmatrix62/echoo-app/releases"
