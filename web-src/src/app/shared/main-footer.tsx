bb2e3bdf7e3c02c9a1545ac6f756d9407ddd7151
Kyle <zs.matrix62@gmail.com>
2022-08-10 16:00:22 +0800

    chores;

--- web-src/src/app/shared/main-footer.tsx
+++ web-src/src/app/shared/main-footer.tsx
@@ -1,8 +1,15 @@
 import { Space } from "@douyinfe/semi-ui";
 import Text from "@douyinfe/semi-ui/lib/es/typography/text";
+const pjson = require('../../../package.json')
 
 export const MainFooter = () => {
 	return <Space className='main-footer' vertical>
-		<Text type='tertiary'>Copyright © 2022 by <b>Echoo.app</b></Text>
+		<Text type='tertiary'>
+			<Space>
+				<span>Copyright © 2022</span>
+				<b>Echoo.app</b>
+				<span>v{pjson.version}</span>
+			</Space>
+		</Text>
 	</Space>;
 };
