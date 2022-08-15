20cca058b185e56488b32043c230b56c9495dcb5
binger_404 <194959593@qq.com>
2022-08-15 22:05:25 +0800

    add unix-time-converter

--- web-src/src/app/consts/tool-sider-configs.tsx
+++ web-src/src/app/consts/tool-sider-configs.tsx
@@ -1,50 +1,65 @@
-import { NavItemProps } from "@douyinfe/semi-ui/lib/es/navigation";
-import { Calendar, CodeBrackets, LinkOne, PayCode } from "@icon-park/react";
-import { ReactNode } from "react";
-import { JsonFormatterBlock } from "../tool-blocks/json-formatter";
-import { Icon } from "@douyinfe/semi-ui";
-import { Base64Serde } from "../tool-blocks/base64serde";
-import { UrlParser } from "../tool-blocks/url-parser";
-import { CrontabParserBlock } from "../tool-blocks/crontab-parser";
+import { NavItemProps } from '@douyinfe/semi-ui/lib/es/navigation'
+import {
+  Calendar,
+  CodeBrackets,
+  LinkOne,
+  PayCode,
+  Time,
+} from '@icon-park/react'
+import { ReactNode } from 'react'
+import { JsonFormatterBlock } from '../tool-blocks/json-formatter'
+import { Icon } from '@douyinfe/semi-ui'
+import { Base64Serde } from '../tool-blocks/base64serde'
+import { UrlParser } from '../tool-blocks/url-parser'
+import { CrontabParserBlock } from '../tool-blocks/crontab-parser'
+import { UnixTimeConverterPage } from '../tool-blocks/unix-time-converter'
 
 type ToolSiderItem = {
-	navItemProps: NavItemProps;
-	node: ReactNode;
-};
+  navItemProps: NavItemProps
+  node: ReactNode
+}
 
 const ToolSiderConfigs: { [key: string]: ToolSiderItem } = {
-	"json-formatter": {
-		navItemProps: {
-			itemKey: "json-formatter",
-			text: "JSON Formatter",
-			icon: <Icon svg={<CodeBrackets theme="outline" />} />,
-		},
-		node: <JsonFormatterBlock />,
-	},
-	"base64-serde": {
-		navItemProps: {
-			itemKey: "base64-serde",
-			text: "Base64 Coders",
-			icon: <Icon svg={<PayCode theme="outline" />} />,
-		},
-		node: <Base64Serde />,
-	},
-	"url-parser": {
-		navItemProps: {
-			itemKey: "url-parser",
-			text: "URL Parser",
-			icon: <Icon svg={<LinkOne theme="outline" />} />,
-		},
-		node: <UrlParser />,
-	},
-	"crontab-parser": {
-		navItemProps: {
-			itemKey: "crontab-parser",
-			text: "Crontab Parser",
-			icon: <Icon svg={<Calendar theme="outline" />} />,
-		},
-		node: <CrontabParserBlock />,
-	}
-};
+  'json-formatter': {
+    navItemProps: {
+      itemKey: 'json-formatter',
+      text: 'JSON Formatter',
+      icon: <Icon svg={<CodeBrackets theme="outline" />} />,
+    },
+    node: <JsonFormatterBlock />,
+  },
+  'base64-serde': {
+    navItemProps: {
+      itemKey: 'base64-serde',
+      text: 'Base64 Coders',
+      icon: <Icon svg={<PayCode theme="outline" />} />,
+    },
+    node: <Base64Serde />,
+  },
+  'url-parser': {
+    navItemProps: {
+      itemKey: 'url-parser',
+      text: 'URL Parser',
+      icon: <Icon svg={<LinkOne theme="outline" />} />,
+    },
+    node: <UrlParser />,
+  },
+  'crontab-parser': {
+    navItemProps: {
+      itemKey: 'crontab-parser',
+      text: 'Crontab Parser',
+      icon: <Icon svg={<Calendar theme="outline" />} />,
+    },
+    node: <CrontabParserBlock />,
+  },
+  'unix-time-converter': {
+    navItemProps: {
+      itemKey: 'unix-time-converter',
+      text: 'Unix Time Converter',
+      icon: <Icon svg={<Time theme="outline" />} />,
+    },
+    node: <UnixTimeConverterPage />,
+  },
+}
 
-export default ToolSiderConfigs;
+export default ToolSiderConfigs
