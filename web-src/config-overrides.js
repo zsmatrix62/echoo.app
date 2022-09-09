64ebf63bb2e50dc86aeba75bdb7c0e4d0fff4cef
KyleHuang <zs.matrix62@gmail.com>
2022-09-09 18:04:23 +0800

    fix: remove bundle analyzer; merged config branch; rever gh-page command changes;

--- web-src/config-overrides.js
+++ web-src/config-overrides.js
@@ -1,6 +1,4 @@
 const SemiWebpackPlugin = require('@douyinfe/semi-webpack-plugin').default
-const BundleAnalyzerPlugin =
-  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const path = require('path')
 
 module.exports = function override(config, _) {
@@ -8,18 +6,6 @@ module.exports = function override(config, _) {
     new SemiWebpackPlugin({
       theme: '@semi-bot/semi-theme-universedesign',
     }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'server',
-      analyzerHost: '127.0.0.1',
-      analyzerPort: 8888,
-      reportFilename: 'report.html',
-      defaultSizes: 'parsed',
-      openAnalyzer: true,
-      generateStatsFile: false,
-      statsFilename: 'stats.json',
-      statsOptions: null,
-      logLevel: 'info',
-    })
   )
 
   //region WASM Support
