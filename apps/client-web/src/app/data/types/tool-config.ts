c301674074ebce186d452bb843e3ae281c8a7f2e
KyleHuang <zs.matrix62@gmail.com>
2023-04-11 22:04:48 +0800

    init;

--- apps/client-web/src/app/data/types/tool-config.ts
+++ apps/client-web/src/app/data/types/tool-config.ts
@@ -1,10 +1,10 @@
 import type { Type } from '@angular/core';
-import { EchooIconNames } from 'libs/angular/echoo-icons/src/lib/types';
+import { EchooIconNames } from 'libs/echoo-icons/src/lib/types';
 
 export interface ToolConfig {
-	title: string;
-	icon?: EchooIconNames;
-	routerLink: string[];
-	component?: Type<unknown>;
-	children?: ToolConfig[];
+  title: string;
+  icon?: EchooIconNames;
+  routerLink: string[];
+  component?: Type<unknown>;
+  children?: ToolConfig[];
 }
