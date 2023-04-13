d36ca85c5d8932fcbcab4b734aca51468822d8a6
KyleHuang <zs.matrix62@gmail.com>
2023-04-13 21:42:24 +0800

    Remove 'sample' field from JsonFormatterSettingsPayload

--- apps/client-web/src/app/features/tools/formatters/formatter-json/settings.ts
+++ apps/client-web/src/app/features/tools/formatters/formatter-json/settings.ts
@@ -3,7 +3,6 @@ import type { ToolSettings } from '../../../..//data/types/tool-config';
 export type JsonFormatterIndention = '1t' | '2s' | '4s' | 'mini';
 
 type JsonFormatterSettingsPayload = {
-  sample: boolean;
   indention: JsonFormatterIndention;
 };
 
@@ -13,7 +12,6 @@ export const JsonFormatterDefaultSettings: ToolSettings = {
   asQueryParams: true,
   storeInLocalStorageKey: 'json-formatter',
   payload: <JsonFormatterSettingsPayload>{
-    sample: true,
     indention: '4s',
   },
 };
